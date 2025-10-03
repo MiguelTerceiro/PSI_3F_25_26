0478a5421d03fc5a833fc22c39c9d779178f8ec2
MiguelTerceiro <miguelterceiro1028@gmail.com>
2025-10-03 15:55:15 +0100

    terceiro commit

--- M17C-10x01/Program.cs
+++ M17C-10x01/Program.cs
@@ -10,7 +10,7 @@ namespace M17C_10x01
     {
         static void Main(string[] args) {               
             Console.WriteLine("Eu sou o Nikolay... o poderosão cá do sitio");
-            Console.WriteLine("Magalhães sabe usar o git");
+            Console.WriteLine("Magalhães sabe usar o git, às vezes...");
         }
     }
 }
