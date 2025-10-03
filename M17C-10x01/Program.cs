aa479f9b2987f552a15793a79053da6573f497a1
MiguelTerceiro <miguelterceiro1028@gmail.com>
2025-10-03 15:49:49 +0100

    Primeiro commit

---
+++ M17C-10x01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17C_10x01
+{
+    internal class Program
+    {
+        static void Main(string[] args) {               
+            Console.WriteLine("Eu sou o Nikolay... o poderosão cá do sitio");
+        }
+    }
+}
